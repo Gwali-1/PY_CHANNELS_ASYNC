389be2f09ae0de851ec44ce7d20d9e9d1da8653a
Gwali <gwalisammatthew2@gmail.com>
2025-08-19 01:23:17 +0000

    module for library errors

---
+++ PyChanAsync/errors.py
@@ -0,0 +1,29 @@
+class ChanError(Exception):
+    """
+    Base exception class for Channel class. Represnt all Channel
+    related exceptions. Catching this is catching all exceptions thrown by a
+    channel.
+    """
+
+    pass
+
+
+class ChannelClosed(Exception):
+    """
+    ChannelClosed Exception thrown when a channel has been closed(usually by a producer)
+    """
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        self.which_chan: object = kwargs.pop("which_chan")
+        super(ChannelClosed, self).__init__(*args, **kwargs)
+
+
+class ChannelFull(Exception):
+    """
+    ChannelFull  exception  thrown when a channel(in this case buffered) is full, and
+    producer does not want to wait.
+    """
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        self.which_chan: object = kwargs.pop("which_chan")
+        super(ChannelFull, self).__init__(*args, **kwargs)
