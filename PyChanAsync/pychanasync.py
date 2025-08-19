156421909386ec226cc322dd9179fa4d7f5f6a49
Gwali <gwalisammatthew2@gmail.com>
2025-08-19 01:14:35 +0000

    implemented base exception classes , channel class , push method partially and boilerplate work

---
+++ PyChanAsync/pychanasync.py
@@ -0,0 +1,92 @@
+import asyncio
+import collections
+from asyncio import Future
+from types import coroutine
+from typing import Any, Self
+
+
+class ChanError(Exception):
+    """
+    Base exception class for Channel class. Represnt all Channel
+    related exceptions. Catching this is catching all exceptions thrown by a
+    channel.
+    """
+
+    pass
+
+
+class ChannelClosed(Exception):
+    """
+    ChannelClosed Exception thrown when a channel has been closed(usually by a producer)
+    """
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        self.which_chan: object = kwargs.pop("which_chan")
+        super(ChannelClosed, self).__init__(*args, **kwargs)
+
+
+class ChannelFull(Exception):
+    """
+    ChannelFull  exception  thrown when a channel(in this case buffered) is full, and
+    producer does not want to wait.
+    """
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        self.which_chan: object = kwargs.pop("which_chan")
+        super(ChannelFull, self).__init__(*args, **kwargs)
+
+
+class Channel:
+    """
+    A channel instance provides a pipeline to stream data between  conccurent tasks scheduled
+    in an event loop.
+
+    :param bound:   In the case of a buffered channel this defines the boundary.
+                    When value is None, which is the default value, the channel is unbuffered.
+
+                    When Channel is unbuffered. receivers block unless there is a ready producer,
+                    likewise consumers will block unless there is a ready receiver.
+
+                    When Channel is buffered. Producers will only  block when the internal buffer is full and
+                    receivers will only block when the buffer is empty.
+
+    """
+
+    def __init__(self, bound: int | None = None) -> None:
+
+        # validate bound
+        if bound is not None and bound < 0:
+            raise ChanError("Channel bound must be > 0")
+
+        self.bound: int | None = bound
+        self.closed: bool = False
+        # self.ready_receivers: deque[Future[Any]] = deque()
+        self.ready_receivers: collections.deque[Future[Any]] = collections.deque()
+        self.ready_producers: collections.deque[Future[Any]] = collections.deque()
+
+        async def push(self: Self, value: Any) -> Future[Any] | None:
+            """
+            Pushes an item into the channel
+
+            If channel is unbuffered, `push` willr return immediately if a ready receiver is avaible.
+            Otherwise it will block and wait for one.
+
+            :param value: The item to push into the channel
+
+
+            """
+
+            if bound is None:
+                if self.ready_receivers:
+                    ready_receiver: Future[Any] = self.ready_receivers.popleft()
+                    ready_receiver.set_result(value)
+
+                else:
+                    ready_producer: Future[Any] = asyncio.Future()
+                    self.ready_producers.append(ready_producer)
+                    return ready_producer
+
+        async def pull():
+            pass
+
+    pass
