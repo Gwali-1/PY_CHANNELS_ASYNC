5203b8b23828f9ddfb08df92bb1620a7946aa9dd
Gwali <gwalisammatthew2@gmail.com>
2025-09-22 01:52:58 +0000

    pipenv version

--- tests/context.py
+++ tests/context.py
@@ -1,1 +1,9 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+print("sys.path:", sys.path)
+print("cwd:", os.getcwd())
+
 from pychanasync import Channel, chanselect
